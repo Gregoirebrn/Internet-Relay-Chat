cd1d70c983b9e12dfb25cef65deaa2109a776738
grebrune <grebrune@student.42lyon.fr>
2024-12-13 15:41:37 +0100

    Error issue when reconnect with Hexchat

--- sources/Channel_Operations/Quit.cpp
+++ sources/Channel_Operations/Quit.cpp
@@ -4,12 +4,15 @@
 
 #include "Channel.hpp"
 
-void	Channel::Quit(int fd_cli) {
-	std::string nick = Get_Client_Name(fd_cli);
+int	Channel::Quit(std::string buff, int fd_cli) {
+	(void)buff;
+	std::string nick = GetName(fd_cli);
 	for (chan_t it = _channel.begin(); it != _channel.end() ; ++it) {
 		if (it->first.find(nick))
 			_channel[it->first].erase(nick);
 	}
 	Remove(fd_cli);
 	send_error(fd_cli, ERR_GONE(nick));
+	std::cout << "QUIT" << std::endl;
+	return (0);
 }
