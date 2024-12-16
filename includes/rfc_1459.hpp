f1d13abc4c0f4d068fb1c241ec42f2d90709ceeb
grebrune <grebrune@student.42lyon.fr>
2024-12-16 19:21:47 +0100

    mod printing

--- includes/rfc_1459.hpp
+++ includes/rfc_1459.hpp
@@ -16,12 +16,12 @@
 
 //nickname
 #define ERR_NONICKNAMEGIVEN "431 :No nickname given\r\n"
-#define ERR_ERRONEUSNICKNAME(nick) ("432 " + (nick) + ":Erroneus nickname\r\n")
-#define ERR_NICKNAMEINUSE(nick) ("433 " + (nick) + ":Nickname is already in use\r\n")
+#define ERR_ERRONEUSNICKNAME(nick) ("432 :" + (nick) + ":Erroneus nickname\r\n")
+#define ERR_NICKNAMEINUSE(nick) ("433 :" + (nick) + ":Nickname is already in use\r\n")
 //for multiple server #define ERR_NICKCOLLISION(nick) ("436 " + (nick) + ":Nickname collision KILL from <user>@<host>\r\n")
 
 //kick
-#define ERR_USERNOTINCHANNEL(client, nick, channel) ("441" + (client) + (nick) + (channel) + " :They aren't on that channel\r\n")
+#define ERR_USERNOTINCHANNEL(client, nick, channel) ("441 :" + (client) + (nick) + (channel) + " :They aren't on that channel\r\n")
 #define RPL_SUCCESKICK(nick, channel, comment) ("Command to kick " + (nick) + " from " + (channel) + " \r\n")
 
 //
@@ -38,23 +38,23 @@
 //#define ERR_UMODEUNKNOWNFLAG
 
 //invite
-#define RPL_INVITING(client, nick, channel) ("341 " + (client) + " " + (nick) + " " + (channel) + "\r\n")
-#define ERR_NOSUCHCHANNEL(channel) ("403 " + (channel) + " :No such channel\r\n")
-#define ERR_NOTONCHANNEL(channel) ("442 " + (channel) + " :You're not on that channel\r\n")
-#define ERR_USERONCHANNEL(nick, channel) ("443 " + (nick) + (channel) + " :is already on channel\r\n")
-#define ERR_CHANOPRIVSNEEDED(client, channel) ("482 " + (client) + (channel) + " :You're not channel operator\r\n")
+#define RPL_INVITING(client, nick, channel) ("341 :" + (client) + " " + (nick) + " " + (channel) + "\r\n")
+#define ERR_NOSUCHCHANNEL(channel) ("403 :" + (channel) + " :No such channel\r\n")
+#define ERR_NOTONCHANNEL(channel) ("442 :" + (channel) + " :You're not on that channel\r\n")
+#define ERR_USERONCHANNEL(nick, channel) ("443 :" + (nick) + (channel) + " :is already on channel\r\n")
+#define ERR_CHANOPRIVSNEEDED(client, channel) ("482 :" + (client) + (channel) + " :You're not channel operator\r\n")
 
 //topic
-#define RPL_NOTOPIC(client, channel) ("331" + (client) + (channel) + " :No topic is set\r\n")
-#define RPL_TOPIC(client, channel, topic) ("332" + (client) + (channel) + " :" + (topic) + "\r\n")
+#define RPL_NOTOPIC(client, channel) ("331 :" + (client) + (channel) + " :No topic is set\r\n")
+#define RPL_TOPIC(client, channel, topic) ((client) + " " + (channel) + " :" + (topic) + "\r\n")
 #define RPL_TOPICWHOTIME(client, channel, nick, setat) ("333" + (client) + " " + (channel) + " " + (nick) + " " + (setat) + "\r\n")
 
 //join
-#define RPL_NAMREPLY(client, channel, prenick) ("353 " + (client) + " = " + (channel) + " :" + (prenick) + "\r\n")
-#define ERR_TOOMANYCHANNELS(client, channel) ("405 " + (client) + " " + (channel) + " :You have joined too many channels\r\n")
-#define ERR_BADCHANNELKEY(client, channel) ("475 " + (client) + " " + (channel) + " :Cannot join channel (+k)\r\n")
-#define RPL_ENDOFNAMES(client, channel) ("366 " + (client) + (channel) + " :End of /NAMES list\r\n")
+#define RPL_NAMREPLY(client, channel, prenick) ("353 :" + (client) + " = " + (channel) + " :" + (prenick) + "\r\n")
+#define ERR_TOOMANYCHANNELS(client, channel) ("405 :" + (client) + " " + (channel) + " :You have joined too many channels\r\n")
+#define ERR_BADCHANNELKEY(client, channel) ("475 :" + (client) + " " + (channel) + " :Cannot join channel (+k)\r\n")
+#define RPL_ENDOFNAMES(client, channel) ("366 :" + (client) + " " + (channel) + " :End of /NAMES list\r\n")
 
 
 //privmsg
-#define ERR_NOSUCHNICK(client , nick) ("401 :" + (client) + ":" + (nick) + ": No such nick\r\n")
+#define ERR_NOSUCHNICK(client , nick) ((client) + " " + (nick) + ": No such nick\r\n")
