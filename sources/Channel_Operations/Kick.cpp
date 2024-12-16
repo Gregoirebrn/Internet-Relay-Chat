1b9353b1b13e723861c9a67de1c2a6b3cb390671
grebrune <grebrune@student.42lyon.fr>
2024-12-16 20:11:55 +0100

    WIP trim to do

--- sources/Channel_Operations/Kick.cpp
+++ sources/Channel_Operations/Kick.cpp
@@ -8,8 +8,14 @@ int Channel::Kick(std::string buff, int fd_cli) {
 	size_t end_wd = buff.find(' ');//search first argument
 	if (std::string::npos == end_wd)
 		return (send_error(fd_cli, ERR_NEEDMOREPARAMS("KICK")), 462);//if we are at the end of the word then the second word is missing
-	std::string channel = buff.substr(0, end_wd);
-	if (!get_rights(GetName(fd_cli), channel, fd_cli))// check if the client caller have the rights to kick
+	size_t two_dots = buff.find(':');//search second argument
+	std::string channel = buff.substr(end_wd + 2, two_dots);
+	std::cout << "KICK_CHANNEL :" << channel << std::endl;
+	if (channel[0] != '#')
+		return (send_error(fd_cli, ERR_NOSUCHCHANNEL(channel)), 403);
+	channel = channel.substr(0, channel.size() -1);
+	std::cout << "KICK_CHANNEL 2:" << channel << std::endl;
+	if (!get_rights(_client->GetName(fd_cli), channel, fd_cli))// check if the client caller have the rights to kick
 		return (404);
 	size_t comma = buff.find(':');// search if a comment is givin
 	std::string comm;
@@ -22,8 +28,12 @@ int Channel::Kick(std::string buff, int fd_cli) {
 	std::string nick = buff.substr(end_wd, comma);
 	user_t found = _channel[channel].find(nick);//search the nickname in the database of the channel
 	if (_channel[channel].end() == found)
-		return (send_error(fd_cli, ERR_USERNOTINCHANNEL(GetName(fd_cli), nick, channel)), 441);// we didn't find him
+		return (send_error(fd_cli, ERR_USERNOTINCHANNEL(_client->GetName(fd_cli), nick, channel)), 441);// we didn't find him
 	_channel.erase(nick);//we did find him
 	send_error(fd_cli, RPL_SUCCESKICK(nick, channel, comment));
 	return (0);
 }
+
+//HEXCHAT
+// << KICK irc_test #chanada :goyo
+// mod to take not the first arg but the second
